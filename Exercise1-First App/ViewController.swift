bb08fb69c5ec452691dcfbb47eefb3eca6789903
Chris Bouquet <Chris@Chris-Bouquets-MacBook-Pro.local>
2015-10-25 11:03:43 -0600

    first git upload

--- Exercise1-First App/ViewController.swift
+++ Exercise1-First App/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redBirdImage: UIImageView!
+    @IBOutlet weak var blueBirdImage: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hideBlueBird(sender: AnyObject) {
+        blueBirdImage.hidden = true
+        redBirdImage.hidden = false
+        
+    }
+    
+    
+    
+    @IBAction func hideRedBird(sender: AnyObject) {
+        redBirdImage.hidden = true
+        blueBirdImage.hidden = false
+        
+        
+        
     }
 
 
